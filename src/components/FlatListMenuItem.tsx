2719c5920ba74479a75116eaeb22f5e3dc8d3fd3
Albert Urbina <johan50693@gmail.com>
2023-06-25 11:10:29 -0400

    Se agrega elemento touchableopacity al menu item

--- src/components/FlatListMenuItem.tsx
+++ src/components/FlatListMenuItem.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { MenuItem } from '../interfaces/appInterfaces';
 import Icon from 'react-native-vector-icons/Ionicons';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 interface Props {
   menuItem: MenuItem;
@@ -10,23 +11,26 @@ interface Props {
 
 export const FlatListMenuItem = ( {menuItem}: Props) => {
   return (
-    <View style= {styles.container}>
+    <TouchableOpacity
+      activeOpacity={0.8}
+    >
+      <View style= {styles.container}>
+          <Icon
+            name= {menuItem.icon}
+            color="gray"
+            size= {23}
+          />
+          <Text style={styles.itemText}>{menuItem.name} </Text>
 
-        <Icon
-          name= {menuItem.icon}
-          color="gray"
-          size= {23}
-        />
-        <Text style={styles.itemText}>{menuItem.name} </Text>
+          <View style={{flex: 1}} />
 
-        <View style={{flex: 1}} />
-
-        <Icon
-          name= "chevron-forward-outline"
-          color="gray"
-          size= {23}
-        />
-    </View>
+          <Icon
+            name= "chevron-forward-outline"
+            color="gray"
+            size= {23}
+          />
+      </View>
+    </TouchableOpacity>
   );
 };
 
