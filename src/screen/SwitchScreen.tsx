c49bb3de2fe5184d6854b4ebcf3da12b7a4a2f4b
Albert Urbina <johan50693@gmail.com>
2023-06-26 19:24:22 -0400

    se crea un custon switch

--- src/screen/SwitchScreen.tsx
+++ src/screen/SwitchScreen.tsx
@@ -1,25 +1,50 @@
 /* eslint-disable prettier/prettier */
 import React, { useState } from 'react';
-import {View, Switch, Platform} from 'react-native';
+import {View, Switch, Platform, Text, StyleSheet} from 'react-native';
 import { HeaderTitle } from '../components/HeaderTitle';
+import { CustonWitch } from '../components/CustonWitch';
 
 export const SwitchScreen = () => {
 
-  const [isEnabled, setIsEnabled] = useState(false);
-  const toggleSwitch = () => setIsEnabled(!isEnabled);
+  const [state, setState] = useState({
+    isActive: true,
+    isHungry: false,
+    isHappy: true,
+  });
+
+  const {isActive,isHappy,isHungry} = state;
+
+  const onChange = (value: boolean, field: string) => {
+    setState({
+      ...state,
+      [field]: value,
+    });
+  };
 
   return (
     <View style={{marginHorizontal: 20}} >
 
        <HeaderTitle title="Switches" />
+      <View style={styles.switchRow}>
+        <Text style={styles.switchText} >isActive</Text>
+        <CustonWitch isOn={isActive} onChange={ (value) => onChange(value,'isActive')} />
+      </View>
 
-       <Switch
-          trackColor={{false: '#d9d9db', true: '#5856d6'}}
-          thumbColor={(Platform.OS === 'android') ? '#5856d6' : ''}
-          // ios_backgroundColor="#3e3e3e"
-          onValueChange={toggleSwitch}
-          value={isEnabled}
-        />
+      <Text style={styles.switchText} >
+        {JSON.stringify(state,null,5)}
+      </Text>
+
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+    switchRow: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+    },
+    switchText: {
+      fontSize: 25,
+    },
+});
