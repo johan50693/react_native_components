38e4af26c02e6ab1a3eab135f687e3c65486146f
Albert Urbina <johan50693@gmail.com>
2023-07-20 12:49:54 -0400

    Se personaliza el pull to refresh

--- src/screen/PulltoRefreshScreen.tsx
+++ src/screen/PulltoRefreshScreen.tsx
@@ -8,13 +8,15 @@ import { styles } from '../theme/appTheme';
 export const PulltoRefreshScreen = () => {
 
   const [refreshing, setRefreshing] = useState(false);
+  const [data, setData] = useState<string>();
 
   const onRefresh = () => {
     setRefreshing(true);
     setTimeout(() => {
       console.log('terminamos');
       setRefreshing(false);
-    }, 1500);
+      setData('hola mundo');
+    }, 2500);
   };
 
   return (
@@ -24,11 +26,18 @@ export const PulltoRefreshScreen = () => {
         <RefreshControl
           refreshing={refreshing}
           onRefresh={onRefresh}
+          progressViewOffset={10}
+          progressBackgroundColor="#5856D6"
+          colors={['white','red','orange']}
         />
       }
     >
       <View style={styles.globalMargin} >
         <HeaderTitle title="Pull to refresh" />
+        {
+          data && <HeaderTitle title={data} />
+        }
+
       </View>
     </ScrollView>
   );
