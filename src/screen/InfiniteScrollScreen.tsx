2c44ebca3eff5f8cb961a14746e0e26af9d700f0
Albert Urbina <johan50693@gmail.com>
2023-07-25 12:01:25 -0400

    Se agregan imagenes al ininity scroll

--- src/screen/InfiniteScrollScreen.tsx
+++ src/screen/InfiniteScrollScreen.tsx
@@ -1,8 +1,7 @@
 /* eslint-disable prettier/prettier */
 import React, {useState} from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet, Image, FlatList, ActivityIndicator } from 'react-native';
 import { HeaderTitle } from '../components/HeaderTitle';
-import { FlatList } from 'react-native-gesture-handler';
 
 export const InfiniteScrollScreen = () => {
 
@@ -15,14 +14,21 @@ export const InfiniteScrollScreen = () => {
     for (let i = 0; i < 5; i++) {
       newArray[i] = numbers.length + i;
     }
-    setNumbers([...numbers, ...newArray]);
+
+    setTimeout(() => {
+      setNumbers([...numbers, ...newArray]);
+    }, 1500);
   };
 
   const renderItem = ( item:number) => {
     return (
-      <Text style={styles.textitem}>
-        {item}
-      </Text>
+      <Image
+        source={{ uri: `https://picsum.photos/id/${item}/200/300`}}
+        style={{
+          width: '100%',
+          height: 400,
+        }}
+      />
     );
   };
 
@@ -37,6 +43,17 @@ export const InfiniteScrollScreen = () => {
           ListHeaderComponent={<HeaderTitle title="Infinite Scroll" />}
           onEndReached={ loadMore}
           onEndReachedThreshold={0.5}
+
+          ListFooterComponent={ () => (
+            <View style={{
+              height: 150,
+              width: '100%',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+              <ActivityIndicator size={50} color="5856d6" />
+            </View>
+          )}
         />
 
       </View>
