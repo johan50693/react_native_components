bbb5c3a2ab8606874f530ae1aa66bba35febf59a
Albert Urbina <johan50693@gmail.com>
2023-07-26 18:41:04 -0400

    Se agrega el boton del ultimo slide

--- src/screen/SlidesScreen.tsx
+++ src/screen/SlidesScreen.tsx
@@ -1,9 +1,14 @@
 /* eslint-disable prettier/prettier */
-import React, {useState} from 'react';
-import { Dimensions, ImageSourcePropType, SafeAreaView, Image, View, Text, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import React, {useState, useRef} from 'react';
+import { Dimensions, ImageSourcePropType, SafeAreaView, Image, View, Text, StyleSheet, Animated } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import Carousel, {Pagination} from 'react-native-snap-carousel';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { useAnimation } from '../hooks/useAnimation';
+import { StackScreenProps } from '@react-navigation/stack';
 
-const {height: screenHeight, width: screenWidth} = Dimensions.get('window');
+const {width: screenWidth} = Dimensions.get('window');
 
 interface Slide {
   title: string;
@@ -29,9 +34,17 @@ const items: Slide[] = [
   },
 ];
 
-export const SlidesScreen = () => {
+interface Props extends StackScreenProps<any, any>{}
+
+
+export const SlidesScreen = ({navigation}:Props )=> {
 
   const [activeIndex, setActiveIndex] = useState(0);
+  const navigationHook = useNavigation();
+  const {opacity, fadeIn} = useAnimation();
+  // const [isVisible, setIsVisible] = useState(false);
+  const isVisible = useRef(false);
+
   const renderItem = (item : Slide) => {
 
     return (
@@ -73,19 +86,72 @@ export const SlidesScreen = () => {
           layout="default"
           onSnapToItem={ (index) => {
             setActiveIndex(index);
+            if (index == 2){
+              // setIsVisible(true);
+              isVisible.current = true;
+              fadeIn();
+            }
           }}
         />
 
-        <Pagination
-          dotsLength={items.length}
-          activeDotIndex={activeIndex}
-          dotStyle={{
-            width: 10,
-            height: 10,
-            borderRadius: 10,
-            backgroundColor: '#5856d6',
-          }}
-        />
+          <View style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            marginHorizontal: 20,
+            alignItems: 'center',
+          }}>
+            <Pagination
+              dotsLength={items.length}
+              activeDotIndex={activeIndex}
+              dotStyle={{
+                width: 10,
+                height: 10,
+                borderRadius: 10,
+                backgroundColor: '#5856d6',
+              }}
+            />
+
+            {
+              isVisible && (
+              <Animated.View
+                style={{
+                  opacity,
+                }}
+              >
+                <TouchableOpacity
+                  style={{
+                    flexDirection: 'row',
+                    backgroundColor: '#5856d6',
+                    width: 140,
+                    height: 50,
+                    borderRadius: 10,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}
+                  activeOpacity={0.9}
+                  onPress={ () => {
+                    if (isVisible.current){
+                      navigation.navigate('HomeScreen'); }
+                    }
+                  }
+                >
+                  <Text style={{
+                    fontSize: 25,
+                    color: 'white',
+                  }}>
+                    Entrar
+                  </Text>
+                  <Icon
+                    name="chevron-forward-outline"
+                    color="white"
+                    size={30}
+                  />
+                </TouchableOpacity>
+              </Animated.View>
+              )
+            }
+          </View>
+
       </SafeAreaView>
   );
 };
